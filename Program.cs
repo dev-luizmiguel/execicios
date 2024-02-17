cf668b51b4ddfb514a0922de46361c66ec8bba74
Luiz Miguel <131730028+dev-luizmiguel@users.noreply.github.com>
2024-02-17 13:48:32 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// Desafio 01
+using System.Net.Http.Headers;
+
+float tempo_viagem=2f;
+float vl_media=92f;
+double  media=(vl_media*tempo_viagem)/12f;
+media = Math.Round(media,3);
+System.Console.WriteLine(media);
